3080962f9d31aec572ba4ac9d54264efda688761
le000nid <l.barayshchuk@gmail.com>
2023-05-23 19:43:04 +0700

    tsunami events

--- all_sketches/initial_noaa_tsunamis.py
+++ all_sketches/initial_noaa_tsunamis.py
@@ -8,32 +8,88 @@ conn = psycopg2.connect(database="postgres", user="postgres",
                         password="password", host="localhost", port=5432)
 cur = conn.cursor()
 
-cur.execute("DROP TABLE IF EXISTS noaa_tsunamis_events")
-cur.execute("CREATE TABLE noaa_tsunamis_events (id SERIAL PRIMARY KEY, " +
-            "area VARCHAR(64), assocDeposits VARCHAR(64), country VARCHAR(64), locationName VARCHAR(64), causeCode INTEGER, damageAmountOrder INTEGER, damageAmountOrderTotal INTEGER, damageMillionsDollars INTEGER, damageMillionsDollarsTotal INTEGER, " +
-            "day INTEGER, deaths INTEGER, deathsAmountOrder INTEGER, deathsAmountOrderTotal INTEGER, deathsTotal INTEGER, earthquakeEventId INTEGER, " +
-            "eventValidity INTEGER, hour INTEGER, housesDamaged INTEGER, housesDamagedAmountOrder INTEGER, housesDamagedAmountOrderTotal INTEGER, housesDamagedTotal INTEGER, " +
-            "housesDestroyed INTEGER, housesDestroyedAmountOrder INTEGER, housesDestroyedAmountOrderTotal INTEGER, housesDestroyedTotal INTEGER, injuries INTEGER, injuriesAmountOrder INTEGER, injuriesAmountOrderTotal INTEGER, " +
-            "injuriesTotal INTEGER, maxWaterHeight INTEGER, minute INTEGER, missing INTEGER, missingAmountOrder INTEGER, missingAmountOrderTotal INTEGER, missingTotal INTEGER, " +
-            "month INTEGER, numDeposits INTEGER, numRunups INTEGER, regionCode INTEGER, second INTEGER, tsIntensity INTEGER, tsMtAbe INTEGER, " +
-            "volcanoEventId INTEGER, year INTEGER, warningStatusId INTEGER, publish BOOL, " +
-            "latitude DOUBLE PRECISION, longitude DOUBLE PRECISION)")
-
 try:
     response = requests.get('http://www.ngdc.noaa.gov/hazel/hazard-service/api/v1/tsunamis/events')
-    # response.raise_for_status()
-    # jsonResponse = response.json()
-    #
-    # insert_list = []
-    # for station in jsonResponse["stations"]:
-    #     id = station['id'] if 'id' in station.keys() else ""
-    #     name = station['name'] if 'name' in station.keys() else ""
-    #     state = station['state'] if 'state' in station.keys() else ""
-    #     lat = station['lat'] if 'lat' in station.keys() else 0.0
-    #     lng = station['lng'] if 'lng' in station.keys() else 0.0
-    #     station_to_insert = (id, name, state, lat, lng)
-    #     insert_list.append(station_to_insert)
-    # cur.executemany("INSERT INTO noaa_tsunamis_events VALUES(%s,%s,%s,%s,%s)", insert_list)
+    response.raise_for_status()
+    jsonResponse = response.json()
+
+    insert_list = []
+    for tsunami in jsonResponse["items"]:
+        noaa_id = tsunami['id'] if 'id' in tsunami.keys() else 0
+        area = tsunami['area'] if 'area' in tsunami.keys() else ""
+        assocDeposits = tsunami['assocDeposits'] if 'assocDeposits' in tsunami.keys() else ""
+        country = tsunami['country'] if 'country' in tsunami.keys() else ""
+        locationName = tsunami['locationName'] if 'locationName' in tsunami.keys() else ""
+        causeCode = tsunami['causeCode'] if 'causeCode' in tsunami.keys() else 0
+        damageAmountOrder = tsunami['damageAmountOrder'] if 'damageAmountOrder' in tsunami.keys() else 0
+        damageAmountOrderTotal = tsunami['damageAmountOrderTotal'] if 'damageAmountOrderTotal' in tsunami.keys() else 0
+        damageMillionsDollars = tsunami['damageMillionsDollars'] if 'damageMillionsDollars' in tsunami.keys() else 0
+        damageMillionsDollarsTotal = tsunami[
+            'damageMillionsDollarsTotal'] if 'damageMillionsDollarsTotal' in tsunami.keys() else 0
+        day = tsunami['day'] if 'day' in tsunami.keys() else 0
+        deaths = tsunami['deaths'] if 'deaths' in tsunami.keys() else 0
+        deathsAmountOrder = tsunami['deathsAmountOrder'] if 'deathsAmountOrder' in tsunami.keys() else 0
+        deathsAmountOrderTotal = tsunami['deathsAmountOrderTotal'] if 'deathsAmountOrderTotal' in tsunami.keys() else 0
+        deathsTotal = tsunami['deathsTotal'] if 'deathsTotal' in tsunami.keys() else 0
+        earthquakeEventId = tsunami['earthquakeEventId'] if 'earthquakeEventId' in tsunami.keys() else 0
+        eventValidity = tsunami['eventValidity'] if 'eventValidity' in tsunami.keys() else 0
+        hour = tsunami['hour'] if 'hour' in tsunami.keys() else 0
+        housesDamaged = tsunami['housesDamaged'] if 'housesDamaged' in tsunami.keys() else 0
+        housesDamagedAmountOrder = tsunami[
+            'housesDamagedAmountOrder'] if 'housesDamagedAmountOrder' in tsunami.keys() else 0
+        housesDamagedAmountOrderTotal = tsunami[
+            'housesDamagedAmountOrderTotal'] if 'housesDamagedAmountOrderTotal' in tsunami.keys() else 0
+        housesDamagedTotal = tsunami['housesDamagedTotal'] if 'housesDamagedTotal' in tsunami.keys() else 0
+        housesDestroyed = tsunami['housesDestroyed'] if 'housesDestroyed' in tsunami.keys() else 0
+        housesDestroyedAmountOrder = tsunami[
+            'housesDestroyedAmountOrder'] if 'housesDestroyedAmountOrder' in tsunami.keys() else 0
+        housesDestroyedAmountOrderTotal = tsunami[
+            'housesDestroyedAmountOrderTotal'] if 'housesDestroyedAmountOrderTotal' in tsunami.keys() else 0
+        housesDestroyedTotal = tsunami['housesDestroyedTotal'] if 'housesDestroyedTotal' in tsunami.keys() else 0
+        injuries = tsunami['injuries'] if 'injuries' in tsunami.keys() else 0
+        injuriesAmountOrder = tsunami['injuriesAmountOrder'] if 'injuriesAmountOrder' in tsunami.keys() else 0
+        injuriesAmountOrderTotal = tsunami[
+            'injuriesAmountOrderTotal'] if 'injuriesAmountOrderTotal' in tsunami.keys() else 0
+        injuriesTotal = tsunami['injuriesTotal'] if 'injuriesTotal' in tsunami.keys() else 0
+        maxWaterHeight = tsunami['maxWaterHeight'] if 'maxWaterHeight' in tsunami.keys() else 0
+        minute = tsunami['minute'] if 'minute' in tsunami.keys() else 0
+        missing = tsunami['missing'] if 'missing' in tsunami.keys() else 0
+        missingAmountOrder = tsunami['missingAmountOrder'] if 'missingAmountOrder' in tsunami.keys() else 0
+        missingAmountOrderTotal = tsunami[
+            'missingAmountOrderTotal'] if 'missingAmountOrderTotal' in tsunami.keys() else 0
+        missingTotal = tsunami['missingTotal'] if 'missingTotal' in tsunami.keys() else 0
+        volcanoEventId = tsunami['volcanoEventId'] if 'volcanoEventId' in tsunami.keys() else 0
+        year = tsunami['year'] if 'year' in tsunami.keys() else 0
+        warningStatusId = tsunami['warningStatusId'] if 'warningStatusId' in tsunami.keys() else 0
+        publish = tsunami['publish'] if 'publish' in tsunami.keys() else False
+        month = tsunami['month'] if 'month' in tsunami.keys() else 0
+        numDeposits = tsunami['numDeposits'] if 'numDeposits' in tsunami.keys() else 0
+        numRunups = tsunami['numRunups'] if 'numRunups' in tsunami.keys() else 0
+        regionCode = tsunami['regionCode'] if 'regionCode' in tsunami.keys() else 0
+        second = tsunami['second'] if 'second' in tsunami.keys() else 0
+        tsIntensity = tsunami['tsIntensity'] if 'tsIntensity' in tsunami.keys() else 0
+        tsMtAbe = tsunami['tsMtAbe'] if 'tsMtAbe' in tsunami.keys() else 0
+        eqDepth = tsunami['eqDepth'] if 'eqDepth' in tsunami.keys() else 0
+        eqMagnitude = tsunami['eqMagnitude'] if 'eqMagnitude' in tsunami.keys() else 0
+        tsMtIi = tsunami['tsMtIi'] if 'tsMtIi' in tsunami.keys() else 0
+        latitude = tsunami['latitude'] if 'latitude' in tsunami.keys() else 0.0
+        longitude = tsunami['longitude'] if 'longitude' in tsunami.keys() else 0.0
+        station_to_insert = (
+            noaa_id, area, assocDeposits, country, locationName, causeCode, damageAmountOrder, damageAmountOrderTotal, damageMillionsDollars,
+            damageMillionsDollarsTotal, day, deaths, deathsAmountOrder, deathsAmountOrderTotal, deathsTotal, earthquakeEventId,
+            eventValidity, hour, housesDamaged, housesDamagedAmountOrder, housesDamagedAmountOrderTotal, housesDamagedTotal,
+            housesDestroyed, housesDestroyedAmountOrder, housesDestroyedAmountOrderTotal, housesDestroyedTotal, injuries, injuriesAmountOrder, injuriesAmountOrderTotal,
+            injuriesTotal, maxWaterHeight, minute, missing, missingAmountOrder, missingAmountOrderTotal, missingTotal,
+            month, numDeposits, numRunups, regionCode, second, tsIntensity, tsMtAbe, volcanoEventId, year, warningStatusId,
+            publish, eqDepth, eqMagnitude, tsMtIi, latitude, longitude)
+        insert_list.append(station_to_insert)
+    cur.executemany("INSERT INTO tsunamis_events(noaa_id, area, assocDeposits, country, locationName, causeCode, damageAmountOrder, damageAmountOrderTotal, damageMillionsDollars, \
+            damageMillionsDollarsTotal, day, deaths, deathsAmountOrder, deathsAmountOrderTotal, deathsTotal, earthquakeEventId,\
+            eventValidity, hour, housesDamaged, housesDamagedAmountOrder, housesDamagedAmountOrderTotal, housesDamagedTotal,\
+            housesDestroyed, housesDestroyedAmountOrder, housesDestroyedAmountOrderTotal, housesDestroyedTotal, injuries, injuriesAmountOrder, injuriesAmountOrderTotal,\
+            injuriesTotal, maxWaterHeight, minute, missing, missingAmountOrder, missingAmountOrderTotal, missingTotal,\
+            month, numDeposits, numRunups, regionCode, second, tsIntensity, tsMtAbe, volcanoEventId, year, warningStatusId,\
+            publish, eqDepth, eqMagnitude, tsMtIi, latitude, longitude) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", insert_list)
 
 
 
--- migrations/20230515_01_y8iuj-initial.py
+++ migrations/20230515_01_y8iuj-initial.py
@@ -7,5 +7,15 @@ steps = [
     step("CREATE TABLE stations (id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY, " +
     "station_id VARCHAR(64), name VARCHAR(64), country VARCHAR(64), state VARCHAR(64), latitude DOUBLE PRECISION, longitude DOUBLE PRECISION, source VARCHAR(64))"),
     step("CREATE TABLE station_values (id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY, " +
-            "station_id uuid, time TIMESTAMP(0), value VARCHAR(64), datum VARCHAR(64), CONSTRAINT fk_station FOREIGN KEY(station_id) REFERENCES stations(id))")
+            "station_id uuid, time TIMESTAMP(0), value VARCHAR(64), datum VARCHAR(64), CONSTRAINT fk_station FOREIGN KEY(station_id) REFERENCES stations(id))"),
+    step("CREATE TABLE tsunamis_events (id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY, " +
+            "noaa_id INTEGER, area VARCHAR(64), assocDeposits VARCHAR(64), country VARCHAR(64), locationName VARCHAR(64), causeCode INTEGER, damageAmountOrder INTEGER, damageAmountOrderTotal INTEGER, damageMillionsDollars INTEGER, damageMillionsDollarsTotal INTEGER, " +
+            "day INTEGER, deaths INTEGER, deathsAmountOrder INTEGER, deathsAmountOrderTotal INTEGER, deathsTotal INTEGER, earthquakeEventId INTEGER, " +
+            "eventValidity INTEGER, hour INTEGER, housesDamaged INTEGER, housesDamagedAmountOrder INTEGER, housesDamagedAmountOrderTotal INTEGER, housesDamagedTotal INTEGER, " +
+            "housesDestroyed INTEGER, housesDestroyedAmountOrder INTEGER, housesDestroyedAmountOrderTotal INTEGER, housesDestroyedTotal INTEGER, injuries INTEGER, injuriesAmountOrder INTEGER, injuriesAmountOrderTotal INTEGER, " +
+            "injuriesTotal INTEGER, maxWaterHeight INTEGER, minute INTEGER, missing INTEGER, missingAmountOrder INTEGER, missingAmountOrderTotal INTEGER, missingTotal INTEGER, " +
+            "month INTEGER, numDeposits INTEGER, numRunups INTEGER, regionCode INTEGER, second INTEGER, tsIntensity INTEGER, tsMtAbe INTEGER, " +
+            "volcanoEventId INTEGER, year INTEGER, warningStatusId INTEGER, publish BOOL, " +
+            "eqDepth INTEGER, eqMagnitude INTEGER, tsMtIi INTEGER, " +
+            "latitude DOUBLE PRECISION, longitude DOUBLE PRECISION)")
 ]
